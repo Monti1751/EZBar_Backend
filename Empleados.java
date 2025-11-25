340707e616728c0200abe4042631d114ba51a3cc
El-Mig <migduquep@gmail.com>
2025-11-25 15:08:42 +0100

    Nuevas clases con nuevos métodos

--- Empleados.java
+++ Empleados.java
@@ -1,99 +1,47 @@
 package EstructuraBD;
 
 public class Empleados {
-	private String empleado_id, nombre_empleado, apellido_empleado, dni, email_empleado, puesto, password, fecha_creacion;
-	private long telefono_empleado;
-	private boolean activo;
-	
-	public Empleados(String empleado_id, String nombre, String apellido, String dni, String email, String puesto, String password, String fecha_creacion, long telefono, boolean activo) {
-		this.empleado_id = empleado_id;
-		this.nombre_empleado = nombre;
-		this.apellido_empleado = apellido;
-		this.dni = dni;
-		this.email_empleado = email;
-		this.puesto = puesto;
-		this.password = password;
-		this.fecha_creacion = fecha_creacion;
-		this.telefono_empleado = telefono;
-		this.activo = activo;
-	}
-	
-	public String getDni() {
-		return dni;
-	}
-	
-	public void setDni(String dni) {
-		this.dni = dni;
-	}
-	
-	public long getTelefonoE() {
-		return telefono_empleado;
-	}
-	
-	public void setTelefonoE(long telefono) {
-		this.telefono_empleado = telefono;
-	}
-	
-	public String getNombreE() {
-		return nombre_empleado;
-	}
-	
-	public void setNombreE(String nombre) {
-		this.nombre_empleado = nombre;
-	}
-	
-	public String getPassword() {
-		return password;
-	}
-	
-	public void setPassword(String password) {
-		this.password = password;
-	}
-	
-	public String getPuesto() {
-		return puesto;
-	}
-	
-	public void setPuesto(String puesto) {
-		this.puesto = puesto;
-	}
-	
-	public String getEmpleado_id() {
-		return empleado_id;
-	}
-	public void setEmpleado_id(String empleado_id) {
-		this.empleado_id = empleado_id;
-	}
-	
-	public String getEmail() {
-		return email_empleado;
-	}
-	
-	public void setEmail(String email) {
-		this.email_empleado = email;
-	}
-	
-	public String getFecha_creacion() {
-		return fecha_creacion;
-	}
-	
-	public void setFecha_creacion(String fecha_creacion) {
-		this.fecha_creacion = fecha_creacion;
-	}
-	
-	public String getApellidoE() {
-		return apellido_empleado;
-	}
-	
-	public void setApellidoE(String apellido) {
-		this.apellido_empleado = apellido;
-	}
-	
-	public boolean isActivo() {
-		return activo;
-	}
-	
-	public void setActivo(boolean activo) {
-		this.activo = activo;
-	}
+    private String empleado_id, nombre_empleado, apellido_empleado, nombre_usuario, dni, puesto_id, password_hash, fecha_creacion;
+    private boolean activo;
+
+    public Empleados(String empleado_id, String nombre_empleado, String apellido_empleado, String nombre_usuario,
+                     String dni, String puesto_id, String password_hash, String fecha_creacion, boolean activo) {
+        this.empleado_id = empleado_id;
+        this.nombre_empleado = nombre_empleado;
+        this.apellido_empleado = apellido_empleado;
+        this.nombre_usuario = nombre_usuario;
+        this.dni = dni;
+        this.puesto_id = puesto_id;
+        this.password_hash = password_hash;
+        this.fecha_creacion = fecha_creacion;
+        this.activo = activo;
+    }
+
+    // Getters y Setters
+    public String getEmpleado_id() { return empleado_id; }
+    public void setEmpleado_id(String empleado_id) { this.empleado_id = empleado_id; }
+
+    public String getNombre_empleado() { return nombre_empleado; }
+    public void setNombre_empleado(String nombre_empleado) { this.nombre_empleado = nombre_empleado; }
+
+    public String getApellido_empleado() { return apellido_empleado; }
+    public void setApellido_empleado(String apellido_empleado) { this.apellido_empleado = apellido_empleado; }
+
+    public String getNombre_usuario() { return nombre_usuario; }
+    public void setNombre_usuario(String nombre_usuario) { this.nombre_usuario = nombre_usuario; }
+
+    public String getDni() { return dni; }
+    public void setDni(String dni) { this.dni = dni; }
+
+    public String getPuesto_id() { return puesto_id; }
+    public void setPuesto_id(String puesto_id) { this.puesto_id = puesto_id; }
+
+    public String getPassword_hash() { return password_hash; }
+    public void setPassword_hash(String password_hash) { this.password_hash = password_hash; }
+
+    public String getFecha_creacion() { return fecha_creacion; }
+    public void setFecha_creacion(String fecha_creacion) { this.fecha_creacion = fecha_creacion; }
+
+    public boolean isActivo() { return activo; }
+    public void setActivo(boolean activo) { this.activo = activo; }
 }
